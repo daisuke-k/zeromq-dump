1e3cf8d048c7dcfd3ce4ec39b5ca33d7c7387268
Daisuke Kotani <kotani@net.ist.i.kyoto-u.ac.jp>
2015-06-02 18:43:29 +0900

    add sub.py: subscribe to the specified publisher, and print the received dict

---
+++ sub.py
@@ -0,0 +1,36 @@
+import argparse
+import logging
+import msgpack
+import zmq
+
+LOG = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--subscribe", help="host to connect for subscribe",
+            required=True, action="append", type=str, metavar="URL")
+    args = parser.parse_args()
+
+    lognum = getattr(logging, "INFO")
+    logging.basicConfig(format='{asctime} {levelname} {module} {message}',
+            style='{', level=lognum)
+
+    context = zmq.Context()
+    poller = zmq.Poller()
+
+    for host in args.subscribe:
+        sock = context.socket(zmq.SUB)
+        sock.connect(host)
+        sock.setsockopt(zmq.SUBSCRIBE, b'')
+        poller.register(sock, zmq.POLLIN)
+
+    while True:
+        try:
+            socks = dict(poller.poll())
+        except KeyboardInterrupt:
+            break
+
+        for sock in socks:
+            recv_msg = msgpack.unpackb(sock.recv(), encoding='utf-8')
+            LOG.info("Received: %s", recv_msg)
+
